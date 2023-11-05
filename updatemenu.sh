9d3bdbcacc83ce18c70a9c709e3abac9fc3eee87
maruX1922 <141104224+maruX1922@users.noreply.github.com>
2023-11-05 21:52:11 +0700

    Update updatemenu.sh

--- updatemenu.sh
+++ updatemenu.sh
@@ -212,11 +212,4 @@ echo -e "\033[0;34m----------------------------------------\033[0m"
 echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
 menu
-;;
-x)
-clear
-echo -e ""
-read -n 1 -s -r -p "Press any key to back on menu"
-menu
-;;
 esac
