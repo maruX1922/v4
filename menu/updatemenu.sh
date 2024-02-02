d7c271243cff896a2c626386a4ec49f4f64781cf
maruX1922 <141104224+maruX1922@users.noreply.github.com>
2024-02-02 11:51:31 +0700

    Update updatemenu.sh

--- menu/updatemenu.sh
+++ menu/updatemenu.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
 MYIP=$(curl -sS ipv4.icanhazip.com)
+clear
+fun_bar() {
+    CMD[0]="$1"
+    CMD[1]="$2"
+    (
+        [[ -e $HOME/fim ]] && rm $HOME/fim
+        ${CMD[0]} -y >/dev/null 2>&1
+        ${CMD[1]} -y >/dev/null 2>&1
+        touch $HOME/fim
+    ) >/dev/null 2>&1 &
+    tput civis
+    echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+    while true; do
+        for ((i = 0; i < 18; i++)); do
+            echo -ne "\033[0;32m#"
+            sleep 0.1s
+        done
+        [[ -e $HOME/fim ]] && rm $HOME/fim && break
+        echo -e "\033[0;33m]"
+        sleep 1s
+        tput cuu1
+        tput dl1
+        echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+    done
+    echo -e "\033[0;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
+    tput cnorm
+}
+
+
+res1() {
 rm -rf menu-trgo
 rm -rf menu-trojan
 rm -rf menu-ssh
 rm -rf menu-backup
-cd /usr/bin
-# menu
 wget -O menu "https://raw.githubusercontent.com/maruX1922/v4/main/menu/menu.sh"
 wget -O menu-vmess "https://raw.githubusercontent.com/maruX1922/v4/main/menu/menu-vmess.sh"
 wget -O menu-vless "https://raw.githubusercontent.com/maruX1922/v4/main/menu/menu-vless.sh"
@@ -97,4 +125,16 @@ chmod +x port-ovpn
 chmod +x xp
 chmod +x acs-set
 chmod +x sshws
+}
+
+clear
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
+echo -e " \e[1;97;101m            UPDATE SCRIPT               \e[0m"
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
+echo -e ""
+echo -e "  \033[1;91m Update Script Service\033[1;37m"
+fun_bar 'res1'
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
+echo -e ""
+read -n 1 -s -r -p "Press [ Enter ] to back on menu"
 menu
